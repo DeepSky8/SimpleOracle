d327cf40843aa96c1852a6d9e8a50f32c4e60728
DeepSky8 <DeepSky8@gmail.com>
2025-08-15 09:07:20 -0400

    Mis-migrated component

--- src/app/display/body/body.ts
+++ src/app/display/body/body.ts
@@ -1,13 +1,13 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { IOracle, oracleType } from '../../data/oracle.model';
 import { oracles } from '../../data/oracles';
 import { YnOracle } from '../../data/yn-oracle/yn-oracle';
 import { Oracle } from '../oracle/oracle';
+import { NgComponentOutlet } from '@angular/common';
 
 @Component({
   selector: 'app-body',
-  imports: [CommonModule],
+  imports: [NgComponentOutlet],
   templateUrl: './body.html',
   styleUrl: './body.scss'
 })
