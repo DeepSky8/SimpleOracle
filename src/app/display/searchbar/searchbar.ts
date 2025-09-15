9cee4cbd4a8544cbf45289c5a8ff1324d81b47fa
DeepSky8 <DeepSky8@gmail.com>
2025-09-15 14:04:38 -0400

    Add search reset button

--- src/app/display/searchbar/searchbar.ts
+++ src/app/display/searchbar/searchbar.ts
@@ -1,20 +1,30 @@
 import { Component } from '@angular/core';
 import { OraclePinService } from '../../data/oracle.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-searchbar',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './searchbar.html',
   styleUrl: './searchbar.scss'
 })
 export class Searchbar {
+  searchText: string = '';
 
-  constructor(private oraclePinService: OraclePinService) { }
+  constructor(private oraclePinService: OraclePinService) {
+
+  }
 
   onInputChange(event: Event): void {
     const input = event.target as HTMLInputElement;
     const value = input?.value || '';
     this.oraclePinService.setSearchText(value)
+  }
+
+  onResetClick(): void {
+    this.searchText = '';
+    this.oraclePinService.clearSearchText();
+
   }
 
 }
